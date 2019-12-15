55d88fcd117b676634caf0d71053bdf75d33f925
Jorge Anez <jorge.anez@coxautoinc.com>
2019-12-14 22:07:14 -0400

    upgrade formik 2.0.8

--- src/components/persons/edit-person/EditPerson.tsx
+++ src/components/persons/edit-person/EditPerson.tsx
@@ -44,7 +44,7 @@ interface IEditProps {
 
 const EditPersonForm: React.FC<IPerson> =  (person) => {
   const [savePerson, { error, data }] = useMutation<{ savePerson: IPerson }, IPerson>(SAVE_PERSON);
-  const { getFieldProps, handleSubmit, errors, touched, isValid } = useFormik<IPerson>({
+  const { values, getFieldProps, getFieldMeta, handleSubmit, errors, touched, isValid } = useFormik<IPerson>({
     initialValues: person,
     isInitialValid: true,
     validationSchema: Yup.object().shape({
@@ -54,38 +54,46 @@ const EditPersonForm: React.FC<IPerson> =  (person) => {
       documentId: Yup.number().required('Invalid entry'),
     }),
     onSubmit: (values, bag) => {
-      console.log(values);
        savePerson({ variables: values });
     }
   });
-  const [firstName, firstNameField] = getFieldProps("firstName", "text");
-  const [lastName, lastNameField] = getFieldProps("lastName", "text");
-  const [documentType, documentTypeField] = getFieldProps("documentType", "text");
-  const [documentId, documentIdField] = getFieldProps("documentId", "number");
+
+  const firstName = getFieldProps("firstName");
+  const firstNameField = getFieldMeta('firstName');
+
+   const lastName = getFieldProps("lastName");
+   const lastNameField = getFieldMeta("lastName");
+
+   const documentType = getFieldProps("documentType");
+   const documentTypeField = getFieldMeta("documentType");
+
+   const documentId = getFieldProps("documentId");
+   const documentIdField = getFieldMeta("documentId");
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div>
           <label>First Name: </label>
           <input placeholder="first name" {...firstName} />
-          {firstNameField.touch && firstNameField.error && (<span>{firstNameField.error}</span>)}
+          {firstNameField.touched && firstNameField.error && (<span>{firstNameField.error}</span>)}
         </div>
         <div>
           <label>Last Name: </label>
           <input placeholder="last name" {...lastName} />
-          {lastNameField.touch && lastNameField.error && (<span>{lastNameField.error}</span>)}
+          {lastNameField.touched && lastNameField.error && (<span>{lastNameField.error}</span>)}
         </div>
 
         <div>
           <label>Document Type </label>
           <input placeholder="Document Type" {...documentType} />
-          {documentTypeField.touch && documentTypeField.error && (<span>{documentTypeField.error}</span>)}
+          {documentTypeField.touched && documentTypeField.error && (<span>{documentTypeField.error}</span>)}
         </div>
 
         <div>
           <label>Document Id </label>
           <input placeholder="Document Id" {...documentId} />
-          {documentIdField.touch && documentIdField.error && (<span>{documentIdField.error}</span>)}
+          {documentIdField.touched && documentIdField.error && (<span>{documentIdField.error}</span>)}
         </div>
 
         <button disabled={!isValid} type="submit">Enviar</button>
